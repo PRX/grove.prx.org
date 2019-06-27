16f7667f7826380e289ab25a21026f8a5a2698a5
Sandi Barr <sandi.barr@prx.org>
2019-06-27 12:51:24 -0500

    fix user image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { concatMap, first, withLatestFrom } from 'rxjs/operators';
 import { AuthService, UserinfoService, Userinfo, HalDoc } from 'ngx-prx-styleguide';
