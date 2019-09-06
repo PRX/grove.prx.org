dcd674b88993ad2293232c3adf0405d72aeded57
Peter St.Jean <peter@stjean.co>
2019-09-05 19:39:44 -0500

    Handle updating, redirecting

--- src/app/campaign/campaign-form.component.ts
+++ src/app/campaign/campaign-form.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../core/user/user.service';
 import { AuguryService } from '../core/augury.service';
-import { withLatestFrom, map } from 'rxjs/operators';
+import { withLatestFrom, map, switchMap } from 'rxjs/operators';
+import { Router, ParamMap, ActivatedRoute } from '@angular/router';
+import { HalDoc } from 'ngx-prx-styleguide';
+import { ToastrService } from 'ngx-prx-styleguide';
+
+interface CampaignData {
+  accountId: number;
+  name: string;
+  type: string;
+  status: string;
+  repName: string;
+  notes: string;
+  advertiserId: number;
+}
 
 @Component({
   selector: 'grove-campaign',
   templateUrl: './campaign-form.component.html',
   styleUrls: ['./campaign-form.component.scss']
 })
-
 export class CampaignFormComponent implements OnInit {
   advertiserOptions$: Observable<{name: string, value: string}[]>;
-  accountOptions$: Observable<{name: string, value: number}[]>;
+  accountOptions$: Observable<{name: string, value: string}[]>;
+  campaignData$: Observable<CampaignData>;
+  campaignDoc$: Observable<HalDoc>;
+
   readonly typeOptions = [
     {name: 'Paid Campaign', value: 'paid_campaign'},
     {name: 'Cross Promo', value: 'cross_promo'},
@@ -34,7 +49,7 @@ export class CampaignFormComponent implements OnInit {
   ];
 
   campaignForm = this.fb.group({
-    accountId: ['', Validators.required],
+    set_account_id: ['', Validators.required],
     name: ['', Validators.required],
     type: ['', Validators.required],
     status: ['', Validators.required],
@@ -42,7 +57,8 @@ export class CampaignFormComponent implements OnInit {
     notes: [''],
     set_advertiser_uri: ['', Validators.required]
   });
-  get accountId() { return this.campaignForm.get('accountId'); }
+
+  get set_account_id() { return this.campaignForm.get('set_account_id'); }
   get name() { return this.campaignForm.get('name'); }
   get type() { return this.campaignForm.get('type'); }
   get status() { return this.campaignForm.get('status'); }
@@ -51,20 +67,67 @@ export class CampaignFormComponent implements OnInit {
   get set_advertiser_uri() { return this.campaignForm.get('set_advertiser_uri'); }
 
   constructor(private fb: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router,
               private userService: UserService,
-              private auguryService: AuguryService) { }
+              private auguryService: AuguryService,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
     this.advertiserOptions$ = this.fetchAdvertisers();
 
+    this.campaignDoc$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.fetchCampaign(params.get('id')))
+    );
+
+    this.campaignData$ = this.formDataFromDoc();
+
+    this.campaignData$.subscribe((result) => result ? this.updateCampaignForm(result) : null);
+
     this.accountOptions$ = this.userService.accounts.pipe(
       withLatestFrom(this.userService.defaultAccount),
       map(([accounts, defaultAccount]) => {
-        const defaultAccountOption = {name: defaultAccount['name'], value: defaultAccount.id};
-        return [defaultAccountOption].concat(accounts.map(doc => ({name: doc['name'], value: doc.id})));
+        return [defaultAccount].concat(accounts).map(acct => ({
+          name: acct['name'],
+          value: [this.auguryService.path, 'accounts', acct['id']].join('/')
+        }));
       })
     );
 
+  }
+
+  formDataFromDoc(): Observable<CampaignData | null> {
+    return this.campaignDoc$.pipe(
+      map((campaign) => {
+        if (campaign === null) { return null; }
+        const { name, type, status, repName, notes } = campaign as any;
+        const [ advertiserId, accountId ] = ['advertiser', 'account'].map(key => this.extractRelationId(key, campaign));
+        return { name, type, status, repName, notes, advertiserId, accountId };
+      })
+    );
+  }
+
+  fetchCampaign(id: string | null): Observable<HalDoc> | Observable<null> {
+    if (id === null) { return of(null); }
+    const resourceName = 'campaign';
+    return this.auguryService.follow(`prx:${resourceName}`, {id});
+  }
+
+  updateCampaignForm({name, type, status, repName, notes, advertiserId, accountId}: CampaignData) {
+    this.campaignForm.patchValue({
+      set_account_id: [this.auguryService.path, 'accounts', accountId].join('/'),
+      name,
+      type,
+      status,
+      repName,
+      notes,
+      set_advertiser_uri: [this.auguryService.path, 'advertisers', advertiserId].join('/')
+    });
+  }
+
+  extractRelationId(key: string, resource: HalDoc) {
+    return resource['_links'][`prx:${key}`]['href'].split('/').pop();
   }
 
   fetchAdvertisers() {
@@ -79,10 +142,20 @@ export class CampaignFormComponent implements OnInit {
   }
 
   campaignFormSubmit() {
-    this.auguryService.root.subscribe(rootDoc => {
-      console.log(rootDoc);
-      const resp = rootDoc.create('prx:campaign', {}, this.campaignForm.value);
-      resp.subscribe(res => console.log(res));
-    });
+    this.campaignDoc$.pipe(
+      withLatestFrom(this.auguryService.root),
+      map(([cmp, root]) => {
+        let resp;
+        if (cmp) {
+          resp = cmp.update(this.campaignForm.value);
+        } else {
+          resp = root.create('prx:campaign', {}, this.campaignForm.value);
+        }
+        resp.subscribe(res => {
+          this.toastr.success('Campaign saved');
+          if (!cmp) { this.router.navigate(['/campaign', res.id]); }
+        });
+      })
+    ).subscribe();
   }
 }
