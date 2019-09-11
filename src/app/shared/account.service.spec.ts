f52aeafe4e6409528686fb26349d83bd249cccbc
cavis <ryancavis@gmail.com>
2019-09-11 11:11:40 -0600

    Accounts service

---
+++ src/app/shared/account.service.spec.ts
@@ -0,0 +1,21 @@
+import { AccountService } from './account.service';
+import { UserServiceMock } from '../core/user/user.service.mock';
+
+describe('AccountService', () => {
+  let user: UserServiceMock;
+  let account: AccountService;
+
+  beforeEach(() => {
+    user = new UserServiceMock();
+    account = new AccountService(<any>user);
+  });
+
+  it('lists accounts', done => {
+    account.listAccounts().subscribe(accounts => {
+      expect(accounts.length).toEqual(2);
+      expect(accounts[0]).toMatchObject({ id: 12 });
+      expect(accounts[1]).toMatchObject({ id: 11 });
+      done();
+    });
+  });
+});
