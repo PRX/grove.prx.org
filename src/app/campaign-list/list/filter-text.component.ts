4881b2d7ab6c39a4cc214e326d5193344fb04c3e
Sandi Barr <sandi.barr@prx.org>
2019-09-11 09:50:46 -0500

    filter text input binding

--- src/app/campaign-list/list/filter-text.component.ts
+++ src/app/campaign-list/list/filter-text.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged, filter } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'grove-filter-text',
   template: `
     <input type="text" placeholder="Search by {{textName}}"
-      [value]="searchText || ''" (keyup)="onChange($event.target.value)">
+      [value]="inputValue || searchText || ''" (keyup)="onChange($event.target.value)">
       <button (click)="search.emit(null)" class="btn-link"><prx-icon size="1em" name="cancel"></prx-icon></button>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,11 +15,17 @@ export class FilterTextComponent implements OnInit, OnDestroy {
   @Input() textName: string;
   @Input() searchText: string;
   @Output() search = new EventEmitter<string>();
+  inputValue: string;
   searchStream = new Subject<string>();
   searchSubcription: Subscription;
   searchOutput$ = this.searchStream.pipe(
+    // binding the input to what the user has typed rather than what gets thru
+    tap(value => this.inputValue = value),
+    // value must be > 2 chars or empty for clearing search
     filter(value => value.length > 2 || value === ''),
+    // are they done typing?
     debounceTime(300),
+    // only if changed
     distinctUntilChanged()
   );
 
