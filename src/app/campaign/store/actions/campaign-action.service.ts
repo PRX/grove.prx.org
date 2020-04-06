cd31161845b01cbfded62ea2b2a0ac822d914260
Sandi Barr <sandi.barr@prx.org>
2020-04-06 16:09:25 -0500

    call this.loadAllocationPreview

--- src/app/campaign/store/actions/campaign-action.service.ts
+++ src/app/campaign/store/actions/campaign-action.service.ts
@@ -153,21 +153,7 @@ export class CampaignActionService implements OnDestroy {
           first()
         )
         .subscribe(flight => {
-          const { createdAt, set_inventory_uri, name, startAt, endAt, zones } = flight;
-          // TODO s/b calling load method for action
-          this.store.dispatch(
-            new allocationPreviewActions.AllocationPreviewLoad({
-              flightId,
-              createdAt,
-              set_inventory_uri,
-              name,
-              startAt,
-              endAt,
-              totalGoal,
-              dailyMinimum,
-              zones
-            })
-          );
+          this.loadAllocationPreview(flight, dailyMinimum);
         });
     }
   }
