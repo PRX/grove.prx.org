39738d1e3d5cfdcf7e0a0a1ac5693b18997db383
Sandi Barr <sandi.barr@prx.org>
2020-05-12 11:59:20 -0500

    use POST on flight preview

--- src/app/core/campaign/flight-preview.service.ts
+++ src/app/core/campaign/flight-preview.service.ts
@@ -22,23 +22,15 @@ export class FlightPreviewService {
     campaignDoc?: HalDoc
   ): Observable<HalDoc[]> {
     if (flightDoc) {
-      // TODO: POST 404s
-      // return flightDoc.create('preview', params, {}).pipe(mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days')));
-      // USING GET/follow
-      return flightDoc.follow('preview', params).pipe(mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days')));
+      return flightDoc.create('preview', {}, params).pipe(mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days')));
     } else if (campaignDoc) {
-      // TODO: POST 404s
-      // return campaignDoc
-      //   .create('prx:flight-allocation-preview', params, {})
-      //   .pipe(mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days')));
-      // USING GET/follow
       return campaignDoc
-        .follow('prx:flight-allocation-preview', params)
+        .create('prx:flight-allocation-preview', {}, params)
         .pipe(mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days')));
     } else {
       return this.augury.root.pipe(
         first(),
-        map(root => root.create('prx:flight-allocation-preview', params, {})),
+        map(root => root.create('prx:flight-allocation-preview', {}, params)),
         mergeMap(flightPreviewDoc => flightPreviewDoc.followList('prx:flight-days'))
       );
     }
