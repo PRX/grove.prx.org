68782b72973ce7fe7cf7218123e9bff09e6904b7
Sandi Barr <sandi.barr@prx.org>
2020-08-25 16:42:19 -0500

    fixes specs

--- src/app/campaign/flight/flight-zones-form.component.spec.ts
+++ src/app/campaign/flight/flight-zones-form.component.spec.ts
@@ -1,8 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule, MatIconModule, MatMenuModule } from '@angular/material';
+import {
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSlideToggleModule
+} from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PingbackFormComponent } from './pingbacks/pingback-form.component';
@@ -54,7 +62,8 @@ describe('FlightZonesFormComponent', () => {
         MatSelectModule,
         MatButtonModule,
         MatIconModule,
-        MatMenuModule
+        MatMenuModule,
+        MatSlideToggleModule
       ],
       declarations: [
         ParentFormComponent,
@@ -86,8 +95,8 @@ describe('FlightZonesFormComponent', () => {
   it('adds zone from the top most available option', () => {
     component.onAddZone();
     expect(component.zones.value).toEqual([
-      { id: '', url: '', pingbacks: [] },
-      { id: component.zoneOptions[component.flightZones.length].id, url: '', pingbacks: [] }
+      { id: '', creativeFlightZones: [] },
+      { id: component.zoneOptions[component.flightZones.length].id, creativeFlightZones: [] }
     ]);
   });
 
@@ -96,11 +105,11 @@ describe('FlightZonesFormComponent', () => {
       zones: [{ id: 'pre_1' }, { id: 'pre_2' }]
     });
     expect(component.zones.value).toEqual([
-      { id: 'pre_1', url: '', pingbacks: [] },
-      { id: 'pre_2', url: '', pingbacks: [] }
+      { id: 'pre_1', creativeFlightZones: [] },
+      { id: 'pre_2', creativeFlightZones: [] }
     ]);
     component.onRemoveZone(0);
-    expect(component.zones.value).toEqual([{ id: 'pre_2', url: '', pingbacks: [] }]);
+    expect(component.zones.value).toEqual([{ id: 'pre_2', creativeFlightZones: [] }]);
   });
 
   it('removes zones when there are more controls than zones', () => {
@@ -108,35 +117,35 @@ describe('FlightZonesFormComponent', () => {
       zones: [{ id: 'pre_1' }, { id: 'pre_2' }]
     });
     expect(component.zones.value).toEqual([
-      { id: 'pre_1', url: '', pingbacks: [] },
-      { id: 'pre_2', url: '', pingbacks: [] }
+      { id: 'pre_1', creativeFlightZones: [] },
+      { id: 'pre_2', creativeFlightZones: [] }
     ]);
-    component.writeValue([{ id: 'pre_2', url: '', pingbacks: [] }]);
-    expect(component.zones.value).toEqual([{ id: 'pre_2', url: '', pingbacks: [] }]);
+    component.writeValue([{ id: 'pre_2' }]);
+    expect(component.zones.value).toEqual([{ id: 'pre_2', creativeFlightZones: [] }]);
   });
 
-  it('does very basic mp3 validations', () => {
-    component.flightZones = [{ id: 'pre_1' }];
+  // it('does very basic mp3 validations', () => {
+  //   component.flightZones = [{ id: 'pre_1' }];
 
-    const zone = component.zones.at(0);
-    const urlField = zone.get('url');
+  //   const zone = component.zones.at(0);
+  //   const urlField = zone.get('url');
 
-    urlField.setValue('');
-    expect(urlField.errors).toEqual(null);
+  //   urlField.setValue('');
+  //   expect(urlField.errors).toEqual(null);
 
-    urlField.setValue('http://this.looks/valid.mp3');
-    expect(urlField.errors).toEqual(null);
-    expect(zone.get('url').hasError('invalidUrl')).toEqual(false);
-    expect(zone.get('url').hasError('notMp3')).toEqual(false);
+  //   urlField.setValue('http://this.looks/valid.mp3');
+  //   expect(urlField.errors).toEqual(null);
+  //   expect(zone.get('url').hasError('invalidUrl')).toEqual(false);
+  //   expect(zone.get('url').hasError('notMp3')).toEqual(false);
 
-    urlField.setValue('ftp://this.is/invalid.mp3');
-    expect(urlField.errors).toEqual({ invalidUrl: { value: 'ftp://this.is/invalid.mp3' } });
-    expect(zone.get('url').hasError('invalidUrl')).toEqual(true);
+  //   urlField.setValue('ftp://this.is/invalid.mp3');
+  //   expect(urlField.errors).toEqual({ invalidUrl: { value: 'ftp://this.is/invalid.mp3' } });
+  //   expect(zone.get('url').hasError('invalidUrl')).toEqual(true);
 
-    urlField.setValue('http://this.is/notaudio.jpg');
-    expect(urlField.errors).toEqual({ notMp3: { value: 'http://this.is/notaudio.jpg' } });
-    expect(zone.get('url').hasError('notMp3')).toEqual(true);
-  });
+  //   urlField.setValue('http://this.is/notaudio.jpg');
+  //   expect(urlField.errors).toEqual({ notMp3: { value: 'http://this.is/notaudio.jpg' } });
+  //   expect(zone.get('url').hasError('notMp3')).toEqual(true);
+  // });
 
   it('does not emit while receiving incoming update', () => {
     jest.spyOn(component, 'onChangeFn');
@@ -147,11 +156,13 @@ describe('FlightZonesFormComponent', () => {
   });
 
   it('clears controls for empty zone', () => {
-    component.writeValue([{ id: 'pre_1', url: 'http://this.looks/valid.mp3' }]);
+    component.writeValue([{ id: 'pre_1', creativeFlightZones: [{ weight: 1 }] }]);
     expect(component.zones.controls[0].get('id').value).toEqual('pre_1');
-    expect(component.zones.controls[0].get('url').value).toEqual('http://this.looks/valid.mp3');
+    expect((component.zones.controls[0].get('creativeFlightZones') as FormArray).controls[0].get('weight').value).toEqual(1);
     component.writeValue([]);
     expect(component.zones.controls[0].get('id').value).toEqual('');
-    expect(component.zones.controls[0].get('url').value).toEqual('');
+    expect(component.zones.controls[0].get('creativeFlightZones').value).toEqual([
+      { creative: null, creativeId: null, enabled: true, weight: 1 }
+    ]);
   });
 });
