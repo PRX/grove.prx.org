7f4172a662b02992ba3c7222f4aa4606e2d0218f
cavis <ryancavis@gmail.com>
2020-10-28 11:53:35 -0600

    Revert typo

--- src/app/campaign/store/selectors/index.ts
+++ src/app/campaign/store/selectors/index.ts
@@ -5,5 +5,4 @@ export * from './campaign-flight.selectors';
 export * from './creative.selectors';
 export * from './flight.selectors';
 export * from './flight-days.selectors';
-export * from './flight-overlap.selectors';
 export * from './inventory.selectors';
