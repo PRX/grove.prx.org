490d383654052dd40d37c40479ebd1a240fb9d0e
cavis <ryancavis@gmail.com>
2020-10-28 11:50:56 -0600

    Service for loading flight overlap

---
+++ src/app/core/campaign/flight-overlap.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HalDoc } from 'ngx-prx-styleguide';
+import { AuguryService } from '../augury.service';
+
+@Injectable()
+export class FlightOverlapService {
+  constructor(private augury: AuguryService) {}
+
+  loadFlightOverlap(filters: string): Observable<HalDoc[]> {
+    return this.augury.followItems('prx:flights', { per: 999, zoom: 0, filters });
+  }
+}
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,6 +2,7 @@ export * from './advertiser/advertiser.service';
 export * from './augury.service';
 export * from './campaign/campaign.service';
 export * from './creative/creative.service';
+export * from './campaign/flight-overlap.service';
 export * from './campaign/flight-preview.service';
 export * from './inventory/inventory.service';
 export * from './user/user.service';
